d77883231123df680d6864039759e236cac0cb33
Ned Twigg <ned.twigg@diffplug.com>
2015-05-23 17:34:17 -0700

    Added OnePerWidget and ColorManager to Durian SWT.

---
+++ src/com/diffplug/common/swt/ColorManager.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import java.util.HashMap;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Widget;
+
+import com.google.common.collect.Maps;
+
+/** Caches colors, and manages their disposal. */
+public class ColorManager {
+	private final HashMap<RGB, Color> colorTable = Maps.newHashMap();
+	private final Display display;
+
+	private ColorManager(Widget parent) {
+		display = parent.getDisplay();
+		parent.addListener(SWT.Dispose, e -> colorTable.values().forEach(Color::dispose));
+	}
+
+	/** Returns a Color for the given RGB value. */
+	public Color getColor(RGB rgb) {
+		Color color = colorTable.get(rgb);
+		if (color == null) {
+			color = new Color(display, rgb);
+			colorTable.put(rgb, color);
+		}
+		return color;
+	}
+
+	/** Returns a ColorManager for the given Control, crating one if necessary. */
+	public static ColorManager forWidget(Widget widget) {
+		return onePerWidget.get(widget);
+	}
+
+	private static OnePerWidget<Widget, ColorManager> onePerWidget = OnePerWidget.from(ColorManager::new);
+}
---
+++ src/com/diffplug/common/swt/OnePerWidget.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Widget;
+
+/**
+ * Simple little class which handles objects that need one-to-one correspondence
+ * with an SWT Widget of some type, but need to be shared.
+ */
+public abstract class OnePerWidget<WidgetType extends Widget, T> {
+	/** Creates a OnePerWidget instance where objects are created using the given function. */
+	public static <WidgetType extends Widget, T> OnePerWidget<WidgetType, T> from(Function<WidgetType, T> creator) {
+		return new OnePerWidget<WidgetType, T>() {
+			@Override
+			protected T create(WidgetType widget) {
+				return creator.apply(widget);
+			}
+		};
+	}
+
+	private Map<WidgetType, T> map = new HashMap<>();
+
+	/** Returns the object for the given control. */
+	public T get(WidgetType ctl) {
+		T value = map.get(ctl);
+		if (value == null) {
+			value = create(ctl);
+			map.put(ctl, value);
+			ctl.addListener(SWT.Dispose, new Listener() {
+				@Override
+				public void handleEvent(Event event) {
+					map.remove(ctl);
+				}
+			});
+		}
+		return value;
+	}
+
+	/** Creates a new object for the control. */
+	protected abstract T create(WidgetType ctl);
+}
