22b3bec792c676399ce94fc868e6cda32c10c88c
Ned Twigg <ned.twigg@diffplug.com>
2015-05-26 08:16:54 -0700

    Added a utility for creating SWT Shells.

---
+++ src/com/diffplug/common/swt/Shells.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+import rx.Subscription;
+import rx.subscriptions.BooleanSubscription;
+
+import com.google.common.base.Preconditions;
+
+/** A fluent builder for creating SWT shells. */
+public class Shells {
+	private final Coat coat;
+	private final int style = SWT.DIALOG_TRIM;
+	private String title = "";
+	private Image image;
+	private Point size;
+	private Point openPosition;
+
+	private Shells(int style, Coat coat) {
+		this.coat = coat;
+	}
+
+	/** Creates a new Shells for this Coat. */
+	public static Shells create(int style, Coat coat) {
+		return new Shells(style, coat);
+	}
+
+	/** Sets the title for this Shell. */
+	public Shells setTitle(String title) {
+		this.title = title;
+		return this;
+	}
+
+	/** Sets the title image for this Shell. */
+	public Shells setImage(Image image) {
+		this.image = image;
+		return this;
+	}
+
+	/**
+	 * Sets the size for this Shell. If it is null, the shell will be packed as
+	 * tightly as possible.  If either component is <= 0, the positive dimension
+	 * will be constrained, and the other dimension will be packed as tightly as
+	 * possible.  */
+	public Shells setSize(Point size) {
+		this.size = size;
+		return this;
+	}
+
+	/** Opens the shell on this parent shell. */
+	public Shell openOn(Shell parent) {
+		Preconditions.checkNotNull(parent);
+		Shell shell = new Shell(parent, style);
+		setupShell(shell);
+		return shell;
+	}
+
+	/** Opens the shell on this parent and blocks. */
+	public void openOnAndBlock(Shell parent) {
+		SwtMisc.loopUntilDisposed(openOn(parent));
+	}
+
+	/** Opens the shell on the currently active shell. */
+	public Shell openOnActive() {
+		Shell shell;
+		Shell parent = Display.getCurrent().getActiveShell();
+		if (parent == null) {
+			shell = new Shell(Display.getCurrent(), style);
+		} else {
+			shell = new Shell(parent, style);
+		}
+		setupShell(shell);
+		return shell;
+	}
+
+	/** Opens the shell on the currently active shell and blocks. */
+	public void openOnActiveBlocking() {
+		SwtMisc.loopUntilDisposed(openOnActive());
+	}
+
+	/** Opens the shell as a root shell. */
+	public Shell openOnDisplay() {
+		Shell shell = new Shell(Display.getCurrent(), style);
+		setupShell(shell);
+		return shell;
+	}
+
+	/** Opens the shell as a root shell and blocks. */
+	public void openOnDisplayBlocking() {
+		SwtMisc.loopUntilDisposed(openOnDisplay());
+	}
+
+	private void setupShell(Shell shell) {
+		// set the text and image
+		if (title != null) {
+			shell.setText(title);
+		}
+		if (image != null) {
+			shell.setImage(image);
+		}
+		// find the composite we're going to draw on
+		Composite userCmp;
+		if (size == null) {
+			userCmp = shell;
+			size = new Point(0, 0);
+		} else {
+			// if there's a specific size, then we'll create a fake one
+			// and set its size appropriately
+			Layouts.setGrid(shell).noMargin();
+			userCmp = new Composite(shell, SWT.NONE);
+			LayoutsGridData gdUtil = Layouts.setGridData(userCmp).grabAll();
+			if (size.x > 0) {
+				gdUtil.widthHint(size.x);
+			}
+			if (size.y > 0) {
+				gdUtil.heightHint(size.y);
+			}
+		}
+		// draw the composite
+		coat.putOn(userCmp);
+
+		// set the opening position
+		if (openPosition == null) {
+			if (shell.getParent() != null) {
+				openPosition = shell.getParent().getLocation();
+				final int SHELL_MARGIN = SwtMisc.systemFontHeight();
+				openPosition.x += SHELL_MARGIN;
+				openPosition.y += SHELL_MARGIN;
+			} else {
+				openPosition = Display.getCurrent().getCursorLocation();
+			}
+		}
+		shell.pack(true);
+
+		// constrain the position by the Display's bounds
+		Rectangle bounds = Display.getCurrent().getBounds();
+		openPosition.x = Math.max(openPosition.x, bounds.x);
+		openPosition.y = Math.max(openPosition.y, bounds.y);
+		openPosition.x = Math.min(openPosition.x + size.x, bounds.x + bounds.width) - size.x;
+		openPosition.y = Math.min(openPosition.y + size.y, bounds.y + bounds.height) - size.y;
+
+		// set the location and open it up!
+		shell.setLocation(openPosition);
+		shell.open();
+	}
+
+	/** Prevents the given shell from closing without prompting.  Returns a Subscription which can cancel this blocking. */
+	public static Subscription confirmClose(Shell shell, String title, String question, Runnable runOnClose) {
+		Listener listener = e -> {
+			e.doit = SwtMisc.blockForQuestion(title, question, shell);
+			if (e.doit) {
+				runOnClose.run();
+			}
+		};
+		shell.addListener(SWT.Close, listener);
+		return BooleanSubscription.create(() -> {
+			SwtExec.immediate().guardOn(shell).exec(() -> {
+				shell.removeListener(SWT.Close, listener);
+			});
+		});
+	}
+}
---
+++ test/com/diffplug/common/swt/ShellsTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import java.util.function.Consumer;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.diffplug.closet.swt.InteractiveTest;
+import com.diffplug.common.swt.Layouts;
+import com.diffplug.common.swt.Shells;
+
+@Category(InteractiveTest.class)
+public class ShellsTest {
+	@Test
+	public void testPack() {
+		testCase("The shell should be a rectangle just large enough to see all the content.", shells -> {
+			shells.setSize(null);
+		});
+	}
+
+	@Test
+	public void testSquare() {
+		testCase("The shell should be a square too small to see all the content.", shells -> {
+			shells.setSize(new Point(100, 100));
+		});
+	}
+
+	@Test
+	public void testWidthFixed() {
+		testCase("The shell should be a skinny rectangle just big enough to see all the content.", shells -> {
+			shells.setSize(new Point(100, 0));
+		});
+	}
+
+	@Test
+	public void testHeightFixed() {
+		testCase("The shell should be a really wide rectangle too short to see all the content.", shells -> {
+			shells.setSize(new Point(0, 50));
+		});
+	}
+
+	private void testCase(String instructions, Consumer<Shells> setSize) {
+		InteractiveTest.testShell(instructions, display -> {
+			Shells underTest = Shells.create(SWT.DIALOG_TRIM, this::longText);
+			setSize.accept(underTest);
+			return underTest.openOnDisplay();
+		});
+	}
+
+	private void longText(Composite parent) {
+		Layouts.setFill(parent);
+
+		Text text = new Text(parent, SWT.WRAP);
+		text.setEditable(false);
+		text.setText("The shell size can be set in the following ways:\n\n" + "- setSize(null): the shell is packed as tight as possible\n" + "- setSize(x, y): the shell is set to size (x, y)\n" + "- setSize(x, 0): the shell is set to width x, and its height is packed as tight as possible\n" + "- setSize(0, y): the shell is set to height y, and its width is packed as tight as possible\n");
+	}
+}
