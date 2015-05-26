b8f5f8a19fb064dce866b3209e90433261c94672
Ned Twigg <ned.twigg@diffplug.com>
2015-05-25 20:54:52 -0700

    Added SwtMisc to Durian SWT.

---
+++ src/com/diffplug/common/swt/SwtMisc.java
@@ -0,0 +1,387 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Widget;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import com.diffplug.common.base.Box;
+import com.diffplug.common.base.Errors;
+import com.diffplug.common.base.TreeDef;
+import com.diffplug.common.base.TreeStream;
+import com.diffplug.common.rx.Rx;
+import com.diffplug.common.swt.os.OS;
+
+/** Miscellaneous SWT functions. */
+public class SwtMisc {
+	/////////////////////
+	// True miscellany //
+	/////////////////////
+	/** Returns true if the flag is set in the given style value */
+	public static boolean flagIsSet(int flag, int style) {
+		return (style & flag) == flag;
+	}
+
+	/** Disposes all children of the given composite. */
+	public static void disposeChildren(Composite cmp) {
+		for (Control child : cmp.getChildren()) {
+			child.dispose();
+		}
+	}
+
+	/** Converts a Runnable into a Listener. */
+	public static Listener asListener(Runnable runnable) {
+		return e -> runnable.run();
+	}
+
+	/**
+	 * Performs an asynchronous layout on the given composite.
+	 * 
+	 * Oftentimes, a layout will not be successful unless it is performed in
+	 * a Display.asyncExec() call, because the current list of events must be
+	 * processed before the layout can take place. 
+	 */
+	public static void asyncLayout(Composite cmp) {
+		SwtExec.async().guardOn(cmp).exec(() -> cmp.layout(true, true));
+	}
+
+	/**
+	 * Performs an asynchronous layout on the given composite anytime that it is resized.
+	 * 
+	 * This can often fix graphical glitches with resize-to-fit layouts, such as a TableColumnLayout.
+	 */
+	public static void asyncLayoutOnResize(Composite cmp) {
+		cmp.addListener(SWT.Resize, e -> asyncLayout(cmp));
+	}
+
+	/** Returns the Display instance, asserting that the method was called from the UI thread. */
+	public static Display assertUI() {
+		Display current = Display.getCurrent();
+		Preconditions.checkArgument(current != null, "Must be called only from UI thread");
+		return current;
+	}
+
+	/** Asserts that the user didn't call this from the UI thread. */
+	public static void assertNotUI() {
+		Display current = Display.getCurrent();
+		Preconditions.checkArgument(current == null, "Must not be called from the UI thread.");
+	}
+
+	////////////////////////////////////////
+	// Run the SWT display loop until ... //
+	////////////////////////////////////////
+	/** Runs the display loop until the given Supplier returns false. */
+	public static void loopUntil(Supplier<Boolean> until) {
+		Display display = assertUI();
+		while (!until.get()) {
+			try {
+				if (!display.readAndDispatch()) {
+					display.sleep();
+				}
+			} catch (Throwable e) {
+				Errors.log().handle(e);
+			}
+		}
+	}
+
+	/** Runs the display loop until the given widget has been disposed. */
+	public static void loopUntilDisposed(Widget widget) {
+		loopUntil(() -> widget.isDisposed());
+	}
+
+	/** Runs the display loop until the given future has returned. */
+	public static <T> T loopUntilGet(ListenableFuture<T> future) throws Throwable {
+		Box<T> result = Box.empty();
+		Box<Throwable> error = Box.empty();
+		Rx.subscribe(future, Rx.onValueOrFailure(result::set, error::set));
+
+		loopUntil(() -> future.isDone());
+
+		if (error.get() != null) {
+			throw error.get();
+		} else {
+			return result.get();
+		}
+	}
+
+	/** Runs the display loop until the given future has returned. */
+	@SuppressWarnings("unchecked")
+	public static <T, E extends Throwable> T loopUntilGetChecked(ListenableFuture<T> future, Class<E> clazz) throws E {
+		try {
+			return loopUntilGet(future);
+		} catch (Throwable error) {
+			if (clazz.isAssignableFrom(error.getClass())) {
+				throw (E) error;
+			} else {
+				throw Errors.asRuntime(error);
+			}
+		}
+	}
+
+	////////////////////////////////////////
+	// Thread-safe blocking notifications //
+	////////////////////////////////////////
+	/** Blocks to notify about a success. Can be called from any thread. */
+	public static void blockForSuccess(String title, String msg, Shell parent) {
+		SwtMisc.blockForMessageBox(title, msg, parent, SWT.ICON_INFORMATION | SWT.OK);
+	}
+
+	/** Blocks to notify about a success. Can be called from any thread. */
+	public static void blockForSuccess(String title, String msg) {
+		blockForSuccess(title, msg, null);
+	}
+
+	/** Blocks to notify about an error. Can be called from any thread. */
+	public static void blockForError(String title, String msg, Shell parent) {
+		SwtMisc.blockForMessageBox(title, msg, parent, SWT.ICON_ERROR | SWT.OK);
+	}
+
+	/** Blocks to notify about an error. Can be called from any thread. */
+	public static void blockForError(String title, String msg) {
+		blockForError(title, msg, null);
+	}
+
+	/** Blocks to ask a yes/no question.  Can be called from any thread. */
+	public static boolean blockForQuestion(String title, String message, Shell parent) {
+		return SwtMisc.blockForMessageBox(title, message, parent, SWT.ICON_QUESTION | SWT.YES | SWT.NO) == SWT.YES;
+	}
+
+	/** Blocks to ask a yes/no question.  Can be called from any thread. */
+	public static boolean blockForQuestion(String title, String message) {
+		return blockForQuestion(title, message, null);
+	}
+
+	/** Blocks to ask an Ok/Cancel question. Can be called from any thread. */
+	public static boolean blockForOkCancel(String title, String message, Shell parent) {
+		return SwtMisc.blockForMessageBox(title, message, parent, SWT.ICON_QUESTION | SWT.OK | SWT.CANCEL) == SWT.OK;
+	}
+
+	/** Blocks to ask an Ok/Cancel question. Can be called from any thread. */
+	public static boolean blockForOkCancel(String title, String message) {
+		return blockForOkCancel(title, message, null);
+	}
+
+	/**
+	 * Opens a message box with the given title, message, and style and returns its result.
+	 * Can be called from any thread.
+	 * 
+	 * @param title The title of the box.
+	 * @param message The message in the box.
+	 * @param parent The parent shell. Null means it will be on the current active shell.
+	 * @param style An OR'ed combination of SWT.YES/NO, SWT.OK/CANCEL, and SWT.ICON_*
+	 * @return The button that was pressed (e.g. SWT.YES, SWT.OK, etc)
+	 */
+	public static int blockForMessageBox(String title, String message, Shell parent, int style) {
+		return SwtExec.blocking().get(() -> {
+			Display display = assertUI();
+			Shell parentInternal;
+
+			if (parent == null) {
+				// null equals display.getActiveShell()
+				parentInternal = display.getActiveShell();
+			} else {
+				parentInternal = parent;
+			}
+
+			boolean parentWasNull = parentInternal == null;
+			if (parentWasNull) {
+				// if there wasn't an active shell, we'll create one, because
+				// MessageBox requires a parent
+				parentInternal = new Shell(Display.getCurrent(), SWT.APPLICATION_MODAL);
+			}
+			MessageBox questionBox = new MessageBox(parentInternal, style);
+			questionBox.setMessage(message);
+			questionBox.setText(title);
+			int result = questionBox.open();
+			if (parentWasNull) {
+				// if we made an invisible parent, clean it up afterwards
+				parentInternal.dispose();
+			}
+			return result;
+		});
+	}
+
+	/**
+	 * Opens a message box with the given title, message, and style and returns its result.
+	 * Can be called from any thread.
+	 * 
+	 * @param title The title of the box.
+	 * @param message The message in the box.
+	 * @param style An OR'ed combination of SWT.YES/NO, SWT.OK/CANCEL, and SWT.ICON_*
+	 * @return The button that was pressed (e.g. SWT.YES, SWT.OK, etc)
+	 */
+	public static int blockForMessageBox(String title, String message, int style) {
+		return blockForMessageBox(title, message, null, style);
+	}
+
+	///////////////////
+	// Scale by font //
+	///////////////////
+	/** The cached height of the system font. */
+	static int systemFontHeight = 0;
+	static int systemFontWidth = 0;
+
+	/** Populates the height and width of the system font. */
+	private static void populateSystemFont() {
+		// create a tiny image to bind our GC to (not that it can't be size 0)
+		Image dummyImg = new Image(assertUI(), 1, 1);
+		GC gc = new GC(dummyImg);
+
+		FontMetrics metrics = gc.getFontMetrics();
+		systemFontHeight = metrics.getHeight();
+		systemFontWidth = metrics.getAverageCharWidth();
+		if (OS.getNative().isMac()) {
+			// add 20% width on Mac
+			systemFontWidth = (systemFontWidth * 12) / 10;
+		}
+
+		gc.dispose();
+		dummyImg.dispose();
+	}
+
+	/** Returns the height of the system font. */
+	public static int systemFontHeight() {
+		if (systemFontHeight == 0) {
+			populateSystemFont();
+		}
+		return systemFontHeight;
+	}
+
+	/** Returns the width of the system font. */
+	public static int systemFontWidth() {
+		if (systemFontWidth == 0) {
+			populateSystemFont();
+		}
+		return systemFontWidth;
+	}
+
+	/** Returns the default width of a button, scaled for the system font. */
+	public static int defaultButtonWidth() {
+		return systemFontWidth() * "   Cancel   ".length();
+	}
+
+	/** Returns the default width of a dialog. */
+	public static int defaultDialogWidth() {
+		return 50 * systemFontWidth();
+	}
+
+	/** Returns a size which is scaled by the system font's height. */
+	public static Point scaleByFontHeight(int cols, int rows) {
+		return new Point(cols * systemFontHeight(), rows * systemFontHeight());
+	}
+
+	/** Returns a dimension which is scaled by the system font's height. */
+	public static int scaleByFontHeight(int rows) {
+		return rows * systemFontHeight();
+	}
+
+	/** Returns a point that represents the size of a (cols x rows) grid of characters printed in the standard system font. */
+	public static Point scaleByFont(int cols, int rows) {
+		return new Point(cols * systemFontWidth(), rows * systemFontHeight());
+	}
+
+	/** Returns a dimension which is guaranteed to be comfortable for the given string. */
+	public static Point scaleByFont(String str) {
+		List<String> lines = splitLines(str);
+		int maxLength = lines.stream().mapToInt(String::length).max().getAsInt();
+		return scaleByFont(maxLength + 2, lines.size());
+	}
+
+	/** Splits a string into a list of lines. Null returns an empty list. */
+	private static List<String> splitLines(String orig) {
+		if (orig == null) {
+			return Collections.emptyList();
+		} else {
+			return Arrays.asList(orig.replace("\\r\\n", "\n").split("\\n"));
+		}
+	}
+
+	//////////////////////
+	// Tree-based stuff //
+	//////////////////////
+	/** Sets the enabled status of every child of the given composite. */
+	public static void setEnabledDeep(Composite root, boolean enabled) {
+		TreeStream.depthFirst(SwtMisc.treeDefControl(), root)
+				// skip the root
+				.filter(ctl -> ctl != root)
+				// set the enabled flag
+				.forEach(ctl -> ctl.setEnabled(enabled));
+	}
+
+	/** TreeDef for Composites. */
+	public static TreeDef.Parented<Composite> treeDefComposite() {
+		return COMPOSITE_TREE_DEF;
+	}
+
+	/** TreeDef for Control. */
+	public static TreeDef.Parented<Control> treeDefControl() {
+		return CONTROL_TREE_DEF;
+	}
+
+	private static final TreeDef.Parented<Composite> COMPOSITE_TREE_DEF = new TreeDef.Parented<Composite>() {
+		@Override
+		public List<Composite> childrenOf(Composite node) {
+			Control[] rawChildren = node.getChildren();
+			List<Composite> children = Lists.newArrayListWithCapacity(rawChildren.length);
+			for (Control child : rawChildren) {
+				if (child instanceof Composite) {
+					children.add((Composite) child);
+				}
+			}
+			return children;
+		}
+
+		@Override
+		public Composite parentOf(Composite node) {
+			return node.getParent();
+		}
+	};
+
+	private static final TreeDef.Parented<Control> CONTROL_TREE_DEF = new TreeDef.Parented<Control>() {
+		@Override
+		public List<Control> childrenOf(Control node) {
+			if (node instanceof Composite) {
+				return Arrays.asList(((Composite) node).getChildren());
+			} else {
+				return Collections.emptyList();
+			}
+		}
+
+		@Override
+		public Control parentOf(Control node) {
+			return node.getParent();
+		}
+	};
+}
---
+++ test/com/diffplug/common/swt/SwtMiscTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import org.eclipse.swt.SWT;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.diffplug.common.swt.SwtMisc;
+
+public class SwtMiscTest {
+	@Test
+	public void flagIsSet() {
+		Assert.assertTrue(SwtMisc.flagIsSet(SWT.NONE, SWT.NONE));
+
+		Assert.assertTrue(SwtMisc.flagIsSet(SWT.BOLD, SWT.BOLD));
+		Assert.assertFalse(SwtMisc.flagIsSet(SWT.ITALIC, SWT.BOLD));
+		Assert.assertFalse(SwtMisc.flagIsSet(SWT.BOLD, SWT.ITALIC));
+		Assert.assertTrue(SwtMisc.flagIsSet(SWT.ITALIC, SWT.ITALIC));
+
+		Assert.assertTrue(SwtMisc.flagIsSet(SWT.NONE, SWT.BOLD | SWT.ITALIC));
+		Assert.assertTrue(SwtMisc.flagIsSet(SWT.BOLD, SWT.BOLD | SWT.ITALIC));
+		Assert.assertTrue(SwtMisc.flagIsSet(SWT.ITALIC, SWT.BOLD | SWT.ITALIC));
+		Assert.assertFalse(SwtMisc.flagIsSet(37, SWT.BOLD | SWT.ITALIC));
+	}
+}
