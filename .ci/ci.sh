a385cb27d36f6bec91eee732245f42a211f594ca
Ned Twigg <ned.twigg@diffplug.com>
2015-05-27 14:20:27 -0700

    Fixed ci.sh.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -3,7 +3,7 @@
 # Do the Gradle build
 ./gradlew build
 
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-swt" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
 	./.ci/publish-artifacts.sh
 	# Push the javadoc
