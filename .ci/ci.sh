992198db3b49fd7d3b47e448a24959d8c9d47995
Ned Twigg <ned.twigg@diffplug.com>
2015-05-28 10:29:26 -0700

    Updated the .ci script so that it fails if the build fails

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Do the Gradle build
-./gradlew build
+./gradlew build || exit 1
 
 if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-swt" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
