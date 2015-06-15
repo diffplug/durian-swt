8036658c3176c0b827091efbef8daa0d65482542
Ned Twigg <ned.twigg@diffplug.com>
2015-06-15 02:27:38 -0700

    Added a useful error checking routine to Coat.

--- src/com/diffplug/common/swt/Coat.java
+++ src/com/diffplug/common/swt/Coat.java
@@ -17,6 +17,8 @@ package com.diffplug.common.swt;
 
 import org.eclipse.swt.widgets.Composite;
 
+import com.google.common.base.Preconditions;
+
 /**
  * A function that can be "put on" a blank {@link Composite}.
  * <p>
@@ -54,5 +56,11 @@ public interface Coat {
 				return returnValue;
 			};
 		}
+	}
+
+	/** Throws an {@link IllegalArgumentException} iff the given {@code Composite} is ready for a coat to be put on. */
+	public static void assertClean(Composite cmp) {
+		Preconditions.checkArgument(cmp.getChildren().length == 0, "The composite should have no children, this had %s.", cmp.getChildren().length);
+		Preconditions.checkArgument(cmp.getLayout() == null, "The composite should have no layout, this had %s.", cmp.getLayout());
 	}
 }
