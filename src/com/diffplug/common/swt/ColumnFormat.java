a8f0ec83f288c19f1710c00c45caac298906e86c
Ned Twigg <ned.twigg@diffplug.com>
2015-06-24 21:11:25 -0700

    Added fluent utilities for creating Table/Tree and their viewers.

---
+++ src/com/diffplug/common/swt/ColumnFormat.java
@@ -0,0 +1,256 @@
+/*
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.eclipse.jface.layout.AbstractColumnLayout;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.layout.TreeColumnLayout;
+import org.eclipse.jface.viewers.ColumnLayoutData;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Item;
+import org.eclipse.swt.widgets.Scrollable;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.Widget;
+
+/** Fluent API for creating {@link Table}s and {@link Tree}s with the specified columns and layout. */
+public class ColumnFormat {
+	/** Creates a {@code TableFormat} with the given style bits. */
+	public static ColumnFormat createWithStyle(int style) {
+		return new ColumnFormat(style);
+	}
+
+	private final int style;
+	private boolean linesVisible = true;
+	private boolean headerVisible = true;
+	private final List<ColumnBuilder> columnBuilders = new ArrayList<>();
+
+	protected ColumnFormat(int style) {
+		this.style = style;
+	}
+
+	/** Returns the style of this format.  Allows utilities that require a certain flag (e.g. SWT.VIRTUAL) to do error-checking. */
+	public int getStyle() {
+		return style;
+	}
+
+	/** Sets the lines to be visible, defaults to true. */
+	public ColumnFormat setLinesVisible(boolean linesVisible) {
+		this.linesVisible = linesVisible;
+		return this;
+	}
+
+	/** Sets the header to be visible, defaults to true. */
+	public ColumnFormat setHeaderVisible(boolean headerVisible) {
+		this.headerVisible = headerVisible;
+		return this;
+	}
+
+	/** Adds a column to the table. */
+	public ColumnBuilder addColumn() {
+		ColumnBuilder builder = new ColumnBuilder();
+		columnBuilders.add(builder);
+		return builder;
+	}
+
+	/** Returns the columns array. */
+	public List<ColumnBuilder> getColumns() {
+		return columnBuilders;
+	}
+
+	/** Builds a {@link Table} with the specified columns and layout. */
+	public Table buildTable(Composite parent) {
+		return buildTable(parent, style, linesVisible, headerVisible, columnBuilders);
+	}
+
+	/** Builds a {@link Tree} with the specified columns and layout. */
+	public Tree buildTree(Composite parent) {
+		return buildTree(parent, style, linesVisible, headerVisible, columnBuilders);
+	}
+
+	/** Builds a table with the given columns. */
+	protected static Table buildTable(Composite parent, int style, boolean linesVisible, boolean headerVisible, List<? extends ColumnBuilder> columnBuilders) {
+		SwtMisc.assertClean(parent);
+		// create the control
+		Table control = new Table(parent, style);
+		control.setLinesVisible(linesVisible);
+		control.setHeaderVisible(headerVisible);
+
+		// create the columns and layout
+		Function<ColumnBuilder, TableColumn> buildFunc = builder -> builder.build(control);
+		List<TableColumn> columns = columnBuilders.stream().map(buildFunc).collect(Collectors.toList());
+		buildLayout(control, new TableColumnLayout(), columns, columnBuilders);
+
+		// return the control
+		return control;
+	}
+
+	/** Builds a table with the given columns. */
+	protected static Tree buildTree(Composite parent, int style, boolean linesVisible, boolean headerVisible, List<? extends ColumnBuilder> columnBuilders) {
+		SwtMisc.assertClean(parent);
+		// create the control
+		Tree control = new Tree(parent, style);
+		control.setLinesVisible(linesVisible);
+		control.setHeaderVisible(headerVisible);
+
+		// create the columns and layout
+		Function<ColumnBuilder, TreeColumn> buildFunc = builder -> builder.build(control);
+		List<TreeColumn> columns = columnBuilders.stream().map(buildFunc).collect(Collectors.toList());
+		buildLayout(control, new TreeColumnLayout(), columns, columnBuilders);
+
+		// return the control
+		return control;
+	}
+
+	/** Builds the layout. */
+	private static void buildLayout(Scrollable control, AbstractColumnLayout layout, List<? extends Widget> columns, List<? extends ColumnBuilder> columnBuilders) {
+		// create the layout
+		for (int i = 0; i < columns.size(); ++i) {
+			layout.setColumnData(columns.get(i), columnBuilders.get(i).dataBuilder.data);
+		}
+		control.getParent().setLayout(layout);
+
+		// update the layout on every resize
+		SwtMisc.asyncLayoutOnResize(control.getParent());
+		// sometimes complicated trees can take a long time to get settled, so we'll do some last-ditch checks
+		Runnable checkLayout = () -> {
+			control.getParent().layout(true, true);
+		};
+		SwtExec.Guarded guarded = SwtExec.async().guardOn(control);
+		guarded.timerExec(500, checkLayout);
+		guarded.timerExec(1000, checkLayout);
+		guarded.timerExec(2000, checkLayout);
+	}
+
+	/** Builder for a single TableColumn. */
+	public static class ColumnBuilder {
+		private String text;
+		private Image image;
+		private int style = SWT.LEFT;
+		ColumnDataBuilder<?> dataBuilder = new ColumnWeightDataBuilder(1);
+
+		protected ColumnBuilder() {}
+
+		TableColumn build(Table parent) {
+			return buildHelper(new TableColumn(parent, style));
+		}
+
+		TreeColumn build(Tree parent) {
+			return buildHelper(new TreeColumn(parent, style));
+		}
+
+		private <T extends Item> T buildHelper(T item) {
+			Optional.ofNullable(text).ifPresent(item::setText);
+			Optional.ofNullable(image).ifPresent(item::setImage);
+			return item;
+		}
+
+		public ColumnBuilder setText(String text) {
+			this.text = text;
+			return this;
+		}
+
+		public ColumnBuilder setImage(Image image) {
+			this.image = image;
+			return this;
+		}
+
+		public ColumnBuilder setStyle(int style) {
+			this.style = style;
+			return this;
+		}
+
+		public ColumnWeightDataBuilder setLayoutWeight(int weight) {
+			ColumnWeightDataBuilder dataBuilder = new ColumnWeightDataBuilder(weight);
+			this.dataBuilder = dataBuilder;
+			return dataBuilder;
+		}
+
+		public ColumnPixelDataBuilder setLayoutPixel(int pixels) {
+			ColumnPixelDataBuilder dataBuilder = new ColumnPixelDataBuilder(pixels);
+			this.dataBuilder = dataBuilder;
+			return dataBuilder;
+		}
+	}
+
+	/** Base class for {@link ColumnWeightDataBuilder} and {@link ColumnPixelDataBuilder}. */
+	private static abstract class ColumnDataBuilder<T extends ColumnLayoutData> {
+		protected final T data;
+
+		protected ColumnDataBuilder(T data) {
+			this.data = data;
+		}
+	}
+
+	/** A fluent API for manipulating a {@link ColumnWeightData}. */
+	public static class ColumnWeightDataBuilder extends ColumnDataBuilder<ColumnWeightData> {
+		private ColumnWeightDataBuilder(int weight) {
+			super(new ColumnWeightData(weight));
+		}
+
+		/** Determines whether the column is resizable. */
+		public ColumnWeightDataBuilder resizable(boolean resizable) {
+			data.resizable = resizable;
+			return this;
+		}
+
+		/**
+		 * Sets the minimum width in pixels.
+		 * <p>
+		 * Default value is 0.
+		 */
+		public ColumnWeightDataBuilder minimumWidth(int minimumWidth) {
+			data.minimumWidth = minimumWidth;
+			return this;
+		}
+	}
+
+	/** A fluent API for manipulating a {@link ColumnPixelData}. */
+	public static class ColumnPixelDataBuilder extends ColumnDataBuilder<ColumnPixelData> {
+		private ColumnPixelDataBuilder(int pixels) {
+			super(new ColumnPixelData(pixels));
+			data.addTrim = true;
+		}
+
+		/** Determines whether the column is resizable. */
+		public ColumnPixelDataBuilder resizable(boolean resizable) {
+			data.resizable = resizable;
+			return this;
+		}
+
+		/**
+		 * Whether to allocate extra width to the column to account for trim taken by the column itself.
+		 * <p>
+		 * Default value is true.
+		 */
+		public ColumnPixelDataBuilder addTrim(boolean addTrim) {
+			data.addTrim = addTrim;
+			return this;
+		}
+	}
+}
---
+++ src/com/diffplug/common/swt/jface/ColumnViewerFormat.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt.jface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.TreeViewerColumn;
+import org.eclipse.jface.viewers.ViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Item;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Tree;
+
+import com.diffplug.common.base.Unhandled;
+import com.diffplug.common.swt.ColumnFormat;
+
+/** Fluent API for creating {@link TableViewer}s and {@link TreeViewer}s with a certain format. */
+public class ColumnViewerFormat<T> {
+	/** Creates a {@code TableFormat} with the given style bits. */
+	public static <T> ColumnViewerFormat<T> createWithStyle(int style) {
+		return new ColumnViewerFormat<T>(style);
+	}
+
+	private final int style;
+	private boolean linesVisible = true;
+	private boolean headerVisible = true;
+	private boolean useHashLookup = true;
+	private final List<ColumnBuilder<T>> columnBuilders = new ArrayList<>();
+
+	private ColumnViewerFormat(int style) {
+		this.style = style;
+	}
+
+	/** Sets the lines to be visible, defaults to true. */
+	public ColumnViewerFormat<T> setLinesVisible(boolean linesVisible) {
+		this.linesVisible = linesVisible;
+		return this;
+	}
+
+	/** Sets the lines to be visible, defaults to true. */
+	public ColumnViewerFormat<T> setHeaderVisible(boolean headerVisible) {
+		this.headerVisible = headerVisible;
+		return this;
+	}
+
+	/**
+	 * Sets the whether the viewer is accelerated by a hashtable, defaults to true.
+	 * 
+	 * @see StructuredViewer#setUseHashlookup(boolean)
+	 * */
+	public ColumnViewerFormat<T> setUseHashLookup(boolean useHashLookup) {
+		this.useHashLookup = useHashLookup;
+		return this;
+	}
+
+	/** Adds a column to the table. */
+	public ColumnBuilder<T> addColumn() {
+		ColumnBuilder<T> builder = new ColumnBuilder<T>();
+		columnBuilders.add(builder);
+		return builder;
+	}
+
+	/** Builds a {@link TableViewer} on the given parent. */
+	public TableViewer buildTable(Composite parent) {
+		Table control = Portal.buildTable(parent, style, linesVisible, headerVisible, columnBuilders);
+		return buildViewer(new TableViewer(control), Arrays.asList(control.getColumns()), TableViewerColumn::new);
+	}
+
+	/** Builds a {@link TreeViewer} on the given parent. */
+	public TreeViewer buildTree(Composite parent) {
+		Tree control = Portal.buildTree(parent, style, linesVisible, headerVisible, columnBuilders);
+		return buildViewer(new TreeViewer(control), Arrays.asList(control.getColumns()), TreeViewerColumn::new);
+	}
+
+	/** Provides a portal to the static methods in ColumnFormat. */
+	private static class Portal extends ColumnFormat {
+		protected Portal() {
+			super(SWT.NONE);
+			throw Unhandled.operationException();
+		}
+
+		protected static Table buildTable(Composite parent, int style, boolean linesVisible, boolean headerVisible, List<? extends ColumnBuilder> columnBuilders) {
+			return ColumnFormat.buildTable(parent, style, linesVisible, headerVisible, columnBuilders);
+		}
+
+		protected static Tree buildTree(Composite parent, int style, boolean linesVisible, boolean headerVisible, List<? extends ColumnBuilder> columnBuilders) {
+			return ColumnFormat.buildTree(parent, style, linesVisible, headerVisible, columnBuilders);
+		}
+	}
+
+	/** Builds a viewer. */
+	private <ViewerType extends StructuredViewer, ColumnType extends Item> ViewerType buildViewer(ViewerType viewer, List<ColumnType> columns, BiFunction<ViewerType, ColumnType, ViewerColumn> columnViewerCreator) {
+		viewer.setUseHashlookup(useHashLookup);
+		for (int i = 0; i < columnBuilders.size(); ++i) {
+			ViewerColumn viewerColumn = columnViewerCreator.apply(viewer, columns.get(i));
+			viewerColumn.setLabelProvider(columnBuilders.get(i).provider);
+		}
+		return viewer;
+	}
+
+	/** Builder for a single TableColumn. */
+	public static class ColumnBuilder<T> extends ColumnFormat.ColumnBuilder {
+		private ColumnBuilder() {}
+
+		private CellLabelProvider provider;
+
+		/** Uses the given as the label provider. */
+		public ColumnBuilder<T> setLabelProvider(CellLabelProvider provider) {
+			this.provider = provider;
+			return this;
+		}
+
+		/** Uses the given function as the textual label provider. */
+		public ColumnBuilder<T> setLabelProviderText(Function<? super T, String> text) {
+			return setLabelProvider(LabelProviders.createWithText(text));
+		}
+
+		/** Uses the given function as the image label provider. */
+		public ColumnBuilder<T> setLabelProviderImage(Function<? super T, Image> image) {
+			return setLabelProvider(LabelProviders.createWithImage(image));
+		}
+
+		/** Uses the given function as the textual and image label provider. */
+		public ColumnBuilder<T> setLabelProviderTextAndImage(Function<? super T, String> text, Function<? super T, Image> image) {
+			return setLabelProvider(LabelProviders.createWithTextAndImage(text, image));
+		}
+
+		/** Manipulate the given builder, and the result will be used as the label provider. */
+		public ColumnBuilder<T> setLabelProviderBuilder(Consumer<? super LabelProviders.Builder<T>> consumer) {
+			LabelProviders.Builder<T> builder = LabelProviders.builder();
+			consumer.accept(builder);
+			return setLabelProvider(builder.build());
+		}
+
+		////////////////////////////////
+		// Override the regular stuff //
+		////////////////////////////////
+		@Override
+		public ColumnBuilder<T> setText(String text) {
+			super.setText(text);
+			return this;
+		}
+
+		@Override
+		public ColumnBuilder<T> setImage(Image image) {
+			super.setImage(image);
+			return this;
+		}
+
+		@Override
+		public ColumnBuilder<T> setStyle(int style) {
+			super.setStyle(style);
+			return this;
+		}
+	}
+}
