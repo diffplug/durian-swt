e459c32ad93304f39c4c757926a864ce03dc90aa
Ned Twigg <ned.twigg@diffplug.com>
2015-11-18 02:01:34 -0800

    Added JFaceRx, which only has toggle for the moment.

---
+++ src/com/diffplug/common/swt/jface/JFaceRx.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt.jface;
+
+import org.eclipse.jface.action.IAction;
+
+import com.google.common.base.Preconditions;
+
+import com.diffplug.common.rx.Rx;
+import com.diffplug.common.rx.RxBox;
+import com.diffplug.common.swt.SwtMisc;
+
+/** Utilities that convert JFace events into Rx-friendly Observables. */
+public class JFaceRx {
+	/**
+	 * Returns an {@code RxBox<Boolean>} for the toggle state of the given action as an RxBox.
+	 * <p>
+	 * Applicable to IAction.AS_CHECK_BOX and AS_RADIO_BUTTON.
+	 */
+	public static RxBox<Boolean> toggle(IAction action) {
+		Preconditions.checkArgument(SwtMisc.flagIsSet(IAction.AS_CHECK_BOX, action.getStyle()) ||
+				SwtMisc.flagIsSet(IAction.AS_RADIO_BUTTON, action.getStyle()));
+		RxBox<Boolean> box = RxBox.of(action.isChecked());
+		action.addPropertyChangeListener(e -> {
+			if ("checked".equals(e.getProperty())) {
+				box.set((Boolean) e.getNewValue());
+			}
+		});
+		Rx.subscribe(box, isChecked -> {
+			action.setChecked(isChecked);
+		});
+		return box;
+	}
+}
---
+++ test/com/diffplug/common/swt/jface/JFaceRxTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt.jface;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.diffplug.common.rx.Rx;
+import com.diffplug.common.rx.RxBox;
+import com.diffplug.common.swt.InteractiveTest;
+import com.diffplug.common.swt.Layouts;
+import com.diffplug.common.swt.jface.Actions.Style;
+
+@Category(InteractiveTest.class)
+public class JFaceRxTest {
+	@Test
+	public void testToggle() {
+		InteractiveTest.testCoat("When you check and uncheck the action, the state of the selected text should change.", 20, SWT.DEFAULT, root -> {
+			Layouts.setGrid(root);
+
+			IAction action = Actions.builder()
+					.setText("Action")
+					.setStyle(Style.CHECK)
+					.build();
+			RxBox<Boolean> selection = JFaceRx.toggle(action);
+
+			{
+				Group cmp = new Group(root, SWT.SHADOW_ETCHED_IN);
+				cmp.setText("Under test");
+				Layouts.setGridData(cmp).grabHorizontal();
+				Layouts.setFill(cmp);
+
+				ToolBarManager manager = new ToolBarManager();
+				manager.createControl(cmp);
+				manager.add(action);
+				manager.update(true);
+			}
+			{
+				Group cmp = new Group(root, SWT.NONE);
+				cmp.setText("Read");
+				Layouts.setGridData(cmp).grabHorizontal();
+				Layouts.setFill(cmp);
+
+				Label selectedTxt = new Label(cmp, SWT.BORDER);
+				Rx.subscribe(selection, s -> selectedTxt.setText(Boolean.toString(s)));
+			}
+			{
+				Group cmp = new Group(root, SWT.NONE);
+				cmp.setText("Write");
+				Layouts.setGridData(cmp).grabHorizontal();
+				Layouts.setFill(cmp);
+
+				Button setFalse = new Button(cmp, SWT.PUSH);
+				setFalse.setText("set false");
+				setFalse.addListener(SWT.Selection, e -> selection.set(false));
+
+				Button setTrue = new Button(cmp, SWT.PUSH);
+				setTrue.setText("set true");
+				setTrue.addListener(SWT.Selection, e -> selection.set(true));
+			}
+		});
+	}
+}
