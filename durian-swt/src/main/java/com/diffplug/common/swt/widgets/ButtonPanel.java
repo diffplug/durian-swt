c3484320eb2611d7b5c82be228b4607aa510c7ea
Ned Twigg <ned.twigg@diffplug.com>
2018-08-01 02:40:07 -0700

    Added ButtonPanel and RadioGroup to durian-swt.

---
+++ durian-swt/src/main/java/com/diffplug/common/swt/widgets/ButtonPanel.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt.widgets;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import com.diffplug.common.base.Preconditions;
+import com.diffplug.common.collect.ImmutableList;
+import com.diffplug.common.collect.Lists;
+import com.diffplug.common.collect.MoreCollectors;
+import com.diffplug.common.swt.Coat;
+import com.diffplug.common.swt.ControlWrapper;
+import com.diffplug.common.swt.Layouts;
+import com.diffplug.common.swt.LayoutsGridData;
+import com.diffplug.common.swt.SwtMisc;
+
+/** A quick way to make right-justified buttons. */
+public class ButtonPanel extends ControlWrapper.AroundControl<Composite> {
+	public static class Builder {
+		private Optional<Coat> leftCoat = Optional.empty();
+		private List<String> texts = Lists.newArrayList();
+		private List<Integer> sizes = Lists.newArrayList();
+		private List<Runnable> actions = Lists.newArrayList();
+
+		/** Adds an OK button. */
+		public Builder leftSide(Coat coat) {
+			Preconditions.checkArgument(!leftCoat.isPresent());
+			leftCoat = Optional.of(coat);
+			return this;
+		}
+
+		/** Adds an OK button. */
+		public Builder ok(Runnable ok) {
+			return add("OK", ok);
+		}
+
+		/** Adds an OK and Cancel button. */
+		public Builder okCancel(Runnable ok, Runnable cancel) {
+			add("OK", ok);
+			add("Cancel", cancel);
+			return this;
+		}
+
+		/** Adds an OK and Cancel button. */
+		public Builder okCancel(Consumer<Boolean> okClicked) {
+			return okCancel(() -> okClicked.accept(true), () -> okClicked.accept(false));
+		}
+
+		/** Adds a button with the given text and action, as well as the default width. */
+		public Builder add(String text, Runnable action) {
+			return add(text, SwtMisc.defaultButtonWidth(), action);
+		}
+
+		/** Adds a button with the given text, width, and action. */
+		public Builder add(String text, int size, Runnable action) {
+			texts.add(text);
+			sizes.add(size);
+			actions.add(action);
+			return this;
+		}
+
+		/** Creates a button panel on the given Composite. */
+		public ButtonPanel build(Composite parent) {
+			return new ButtonPanel(parent, this);
+		}
+
+		/** Creates a button panel on the given Composite, and returns a LayoutsGridData for manipulating it. */
+		public LayoutsGridData buildOnGrid(Composite parent) {
+			return Layouts.setGridData(build(parent));
+		}
+	}
+
+	/** Creates a ButtonPanel builder. */
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	final ImmutableList<Button> buttons;
+
+	protected ButtonPanel(Composite parent, Builder builder) {
+		super(new Composite(parent, SWT.NONE));
+		Layouts.setGrid(wrapped).numColumns(builder.texts.size() + 1).margin(0);
+		if (builder.leftCoat.isPresent()) {
+			Composite leftSide = new Composite(wrapped, SWT.NONE);
+			Layouts.setGridData(leftSide).grabHorizontal();
+			builder.leftCoat.get().putOn(leftSide);
+		} else {
+			Layouts.newGridPlaceholder(wrapped).grabHorizontal();
+		}
+		ImmutableList.Builder<Button> buttonsBuilder = ImmutableList.builder();
+		for (int i = 0; i < builder.texts.size(); ++i) {
+			Button button = new Button(wrapped, SWT.PUSH);
+			buttonsBuilder.add(button);
+			button.setText(builder.texts.get(i));
+			Layouts.setGridData(button).widthHint(builder.sizes.get(i));
+			Runnable action = builder.actions.get(i);
+			button.addListener(SWT.Selection, e -> action.run());
+		}
+		buttons = buttonsBuilder.build();
+	}
+
+	public ImmutableList<Button> getButtons() {
+		return buttons;
+	}
+
+	public Button getButton(String text) {
+		return MoreCollectors.singleOrEmptyShortCircuiting(buttons.stream().filter(b -> text.equals(b.getText()))).get();
+	}
+
+	public void setTexts(String... texts) {
+		Preconditions.checkArgument(buttons.size() == texts.length);
+		for (int i = 0; i < texts.length; ++i) {
+			buttons.get(i).setText(texts[i]);
+		}
+	}
+}
---
+++ durian-swt/src/main/java/com/diffplug/common/swt/widgets/RadioGroup.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.swt.widgets;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import com.diffplug.common.base.Preconditions;
+import com.diffplug.common.collect.BiMap;
+import com.diffplug.common.collect.HashBiMap;
+import com.diffplug.common.collect.ImmutableMap;
+import com.diffplug.common.collect.Iterables;
+import com.diffplug.common.collect.Maps;
+import com.diffplug.common.rx.RxBox;
+import com.diffplug.common.swt.Coat;
+import com.diffplug.common.swt.Layouts;
+import com.diffplug.common.swt.SwtExec;
+
+public class RadioGroup<T> {
+	private final Optional<RxBox<T>> source;
+	private LinkedHashMap<String, T> options = Maps.newLinkedHashMap();
+
+	/** Guava-style constructor. */
+	public static <T> RadioGroup<T> create() {
+		return new RadioGroup<T>(Optional.empty());
+	}
+
+	/** Guava-style constructor. */
+	public static <T> RadioGroup<T> create(RxBox<T> source) {
+		return new RadioGroup<T>(Optional.of(source));
+	}
+
+	protected RadioGroup(Optional<RxBox<T>> source) {
+		this.source = source;
+	}
+
+	public RadioGroup<T> addOption(T value, String name) {
+		options.put(name, value);
+		return this;
+	}
+
+	public RadioGroup<T> addOptions(Iterable<T> values, Function<T, String> labelProvider) {
+		for (T value : values) {
+			addOption(value, labelProvider.apply(value));
+		}
+		return this;
+	}
+
+	public Coat.Returning<RxBox<T>> getCoat() {
+		Preconditions.checkArgument(options.size() > 0, "Must be at least one option!");
+		ImmutableMap<String, T> options = ImmutableMap.copyOf(this.options);
+		return new Coat.Returning<RxBox<T>>() {
+			@Override
+			public RxBox<T> putOn(Composite cmp) {
+				// object which will hold the current selection
+				RxBox<T> selection = source.orElseGet(() -> RxBox.of(Iterables.get(options.values(), 0)));
+
+				// mapping from button to objects
+				BiMap<Button, T> mapping = HashBiMap.create();
+
+				Layouts.setFill(cmp).vertical().margin(0);
+				for (Map.Entry<String, T> entry : options.entrySet()) {
+					// create a button
+					Button btn = new Button(cmp, SWT.RADIO);
+					btn.setText(entry.getKey());
+					// update the mapping
+					mapping.put(btn, entry.getValue());
+					// update selection when a button is clicked
+					btn.addListener(SWT.Selection, e -> {
+						selection.set(mapping.get(btn));
+					});
+				}
+
+				// when the selection is changed, set the button
+				Button firstBtn = Iterables.get(mapping.keySet(), 0);
+				SwtExec.immediate().guardOn(firstBtn).subscribe(selection.asObservable(), obj -> {
+					Button selectedBtn = mapping.inverse().get(obj);
+					for (Button btn : mapping.keySet()) {
+						btn.setSelection(btn == selectedBtn);
+					}
+				});
+				return selection;
+			}
+		};
+	}
+}
